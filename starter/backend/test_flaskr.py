c7a82f44915ce58a5eb5de885d7539b97f8d15fd
somas2015 <somac4job@gmail.com>
2021-01-07 12:13:05 -0500

    first commit

---
+++ starter/backend/test_flaskr.py
@@ -0,0 +1,85 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia"
+        self.database_path = "postgres://{}:{}@{}/{}".format('caryn','postgre','localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+        self.new_question = {
+            'question' : 'What country has the longest coastline in the world?',
+            'answer': 'Canada',
+            'difficulty' : '4',
+            'category' : '3'
+        }
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+    # def test_paginated_questions(self):
+    #     res = self.client().get('/questions/')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code,200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertTrue(data['questions'])
+    #     self.assertTrue(data['total_questions'])
+
+
+    # def test_add_questions(self):
+    #     res = self.client().post('/add_questions/',json = self.new_question)
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code,200)
+    #     self.assertEqual(data['success'],True)
+    #     self.assertTrue(data['questions'])
+    #     self.assertTrue(data['total_questions'])
+
+    # def test_delete_question(self):
+    #     res = self.client().delete('/questions/31')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code,200)
+    #     self.assertEqual(data['success'],True)
+    #     self.assertTrue(data['deleted'])
+    #     self.assertTrue(data['questions'])
+    #     self.assertTrue(data['total_questions'])
+
+    def test_play_quiz(self):
+        res = self.client().post('/questions/play_quiz', json = {'category':'4'})
+        
+
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['question'])
+        self.assertTrue(data['previous_question'])
+        
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
